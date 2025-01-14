eb18c37534bcca74663b3ce6c81c4e506286ec28
kartik <kartikyadav0999@gmail.com>
2025-01-15 02:13:58 +0530

    project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
+import Navbar from "@/scenes/navbar"
+import { useEffect, useState } from "react"
+import { SelectedPage } from "@/shared/types"
+import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
+import OurClasses from "./scenes/ourClasses";
+import ContactUs from "./scenes/contactUs";
+import Footer from "@/scenes/footer";
+
+
 function App() {
 
+  const[selectedPage, setSelectedPage]=useState<SelectedPage>(            //useState<SelectedPage> ->This ensures that the state will only accept the specific string values defined in SelectedPage.
+    SelectedPage.Home   
+  );
+
+  const[isTopOfPage, setIsTopOfPage]= useState<boolean>(true)
+
+  useEffect(()=>{
+    const handleScroll = () =>{
+      if(window.scrollY === 0){                                  //it means it is at top of a page currently without scroll
+        setIsTopOfPage(true)
+        setSelectedPage(SelectedPage.Home)                       //whenever on top we want home to be red color active.
+      }
+      else if(window.scrollY !== 0){
+        setIsTopOfPage(false)
+      }
+    }
+    window.addEventListener("scroll", handleScroll)
+      return ()=> window.removeEventListener("scroll", handleScroll)
+  },[])
 
   return (
-   <div className="app bg-gray-50">app</div>
+  <div className="app bg-gray-50">
+    <Navbar 
+    isTopofPage= {isTopOfPage}
+    selectedPage={selectedPage} 
+    setSelectedPage={setSelectedPage}
+    />
+    <Home setSelectedPage= {setSelectedPage}/>
+    <Benefits setSelectedPage= {setSelectedPage}/>
+    <OurClasses setSelectedPage= {setSelectedPage}/>
+    <ContactUs setSelectedPage= {setSelectedPage} />
+    <Footer/>
+  </div>
   )
 }
 
---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,18 @@
+
+
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-6 mt-8">
+      <div className="text-center">
+        <p>copyright © welcome to my portfolio website</p>
+        <p>
+          <b>Designed by Kartik</b>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
